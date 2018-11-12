399ca6e9b4bef6e3b67649588ef608234e3ffbb6
IoanaBejenaru <44978610+IoanaBejenaru@users.noreply.github.com>
2018-11-12 19:03:13 +0200

    Add files via upload

---
+++ cmmdc.3.cpp..cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a=0,b,i,z=0,n
+    cin>>n
+    for(i=1;i<=n;i++)
+    {
+
+        cin<<A;
+        b=z
+        if(a==b)
+        {
+
+            z=a;
+        }
+        elseif
+        (a==0;b==0)
+        {
+            if(a)
+            {
+                z=a;}
+                else{
+                    z=b;}
+        }
+            }
+            else{while(a!=b)
+                {
+
+                    if(a>b)
+                        a-=b;
+                    elseb-=a;
+                }
+                z=a
+            }
+        }
+        cout<<z
+    }
+}
